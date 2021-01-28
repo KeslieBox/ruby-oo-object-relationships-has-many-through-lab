6f61e6d929e5dc3d9de16b1be1ee1fd80c2e24d0
KeslieBox <kesliebox@gmail.com>
2021-01-27 20:58:27 -0600

    Done.

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,35 @@
 
+class Patient
+attr_accessor 
+attr_reader :name
+
+@@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, doctor)
+        # binding.pry
+        Appointment.new(date, self, doctor)
+    end
+
+    def appointments
+        Appointment.all.select do |appointment|
+            # binding.pry
+            self == appointment.patient
+        end
+    end
+
+    def doctors
+        Appointment.all.collect do |appointment|
+            self == appointment.patient
+            appointment.doctor
+        end
+    end
+end
